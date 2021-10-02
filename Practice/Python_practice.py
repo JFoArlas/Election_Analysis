4918e677534a54796479c74dc7c28a30a92bde7c
JFo <88628933+JFoArlas@users.noreply.github.com>
2021-10-02 11:37:09 -0600

    added resources folder & moved practice files to practice folder

---
+++ Practice/Python_practice.py
@@ -0,0 +1,17 @@
+#What is the score?
+score = int(input("What is your test score? "))
+
+# Determine the grade.
+if score >= 90:
+    print('Your grade is an A.')
+else:
+    if score >= 80:
+        print('Your grade is a B.')
+    else:
+        if score >= 70:
+            print('Your grade is a C.')
+        else:
+            if score >= 60:
+                print('Your grade is a D.')
+            else:
+                print('Your grade is an F.')
