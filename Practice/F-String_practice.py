6962ecc4b120d090716f6b38f2afcc82983ecf42
JFoArlas <88628933+JFoArlas@users.noreply.github.com>
2021-10-03 12:04:03 -0600

    Delete Practice Directory

--- Practice/F-String_practice.py
+++
@@ -1,3 +0,0 @@
-my_votes = int(input("How many votes did you get in the election? "))
-total_votes = int(input("What is the total votes in the election? "))
-print(f"I received {my_votes / total_votes *100}% of the total votes")
